a2744ea8166c42d3dbb806b59b6b1f26d0ca4d4a
BSCE20004_Farhan <73163937+farhan26azam@users.noreply.github.com>
2023-07-02 19:19:24 +0500

    Project Completed

---
+++ sections/World.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable quotes */
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { staggerContainer, fadeIn } from "../utils/motion";
+import { TitleText, TypingText } from "../components";
+
+const World = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People in the World " textStyles="text-center" />
+      <TitleText
+        title={
+          <>
+            Track friends around you and invite them to play together in the
+            same world
+          </>
+        }
+      />
+      <motion.div variant={fadeIn('up', 'tween', 0.3, 1)} className="relative mt-[68px] flex w-full h0[550px]"> 
+        <img src='/map.png' alt="map" className="w-full h-full object-cover">
+        </img>
+        <div className="absolute top-10 left-20 w-[70px] p-[6px] rounded-full bg-[#5D6680]">
+          <img src='people-02.png' alt='people' className="w-full h-full"/>
+        </div>
+        <div className="absolute bottom-20 right-20 w-[70px] p-[6px] rounded-full bg-[#5D6680]">
+          <img src='people-01.png' alt='people' className="w-full h-full"/>
+        </div>
+        <div className="absolute top-1/2 left-[45%] w-[70px] p-[6px] rounded-full bg-[#5D6680]">
+          <img src='people-03.png' alt='people' className="w-full h-full"/>
+        </div>
+      </motion.div>
+    </motion.div>
+  </section>
+);
+
+export default World;
