d4162b27edc041a70d2f39bdb714a18df1590379
BSCE20004_Farhan <73163937+farhan26azam@users.noreply.github.com>
2023-07-02 19:32:16 +0500

    Out folder

---
+++ out/_next/static/NMLozsPNmg3MzFaCRNT9G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/910-b79b5d8c24e05cc6.js","static/chunks/pages/index-b0924d364fd61064.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
